bb38e151758e3541e508b40bf371ba25c5f9b486
Bagda@Sh <168397146+Ashwastaken78867@users.noreply.github.com>
2025-01-28 10:25:28 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,53 @@
 
 import React from "react";
 import './../styles/App.css';
-
+import React, { useState } from "react";
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+  const [todos, setTodos] = useState([]);
+  const [task, setTask] = useState("");
 
-export default App
+  const handleAddTodo = () => {
+    if (task.trim() !== "") {
+      setTodos([...todos, task]);
+      setTask("");
+    }
+  };
+
+  const handleDeleteTodo = (index) => {
+    const newTodos = todos.filter((_, i) => i !== index);
+    setTodos(newTodos);
+  };
+
+  return (
+    <div className="app-container">
+      <h1 className="header">To-Do List</h1>
+      <div className="input-container">
+        <input
+          type="text"
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+          placeholder="Enter a task"
+          className="task-input"
+        />
+        <button onClick={handleAddTodo} className="add-button">
+          Add Todo
+        </button>
+      </div>
+      <ul className="todo-list">
+        {todos.map((todo, index) => (
+          <li key={index} className="todo-item">
+            <span className="todo-text">{todo}</span>
+            <button
+              onClick={() => handleDeleteTodo(index)}
+              className="delete-button"
+            >
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
